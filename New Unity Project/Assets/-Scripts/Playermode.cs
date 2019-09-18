91c4e52703cc8205664ab6ef89e83b3304f35388
Liam <liahm2000@gmail.com>
2019-09-19 11:54:57 +1200

    Script setup

---
+++ New Unity Project/Assets/-Scripts/Playermode.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playermode : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            //Commit Fission
+            //Destroy Default player GameObject, Instantiate Cold and Hot player GameObjects
+        }
+    }
+}
