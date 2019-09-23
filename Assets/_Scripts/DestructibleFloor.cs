f97aeafcaa108713c747a1d66847e484db04a99b
Liam <liamh2000@gmail.com>
2019-09-24 10:00:37 +1200

    Interactions completed
    Yeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeet

--- Assets/_Scripts/DestructibleFloor.cs
+++ Assets/_Scripts/DestructibleFloor.cs
@@ -7,20 +7,62 @@ public class DestructibleFloor : MonoBehaviour
     private bool touching = false;
     public int timerMax = 60;
     private int timer;
+    private Renderer objectRenderer;
+    private Color terrainColor;
 
     private void Start()
     {
+        terrainColor = new Color(0.245f, 0.181f, 0.101f);
+        objectRenderer = gameObject.GetComponent<Renderer>();
         timer = timerMax;
     }
 
     private void Update()
     {
+        
+
         if(touching == true)
         {
             timer--;
         }
 
-        if(timer== 0)
+        if (timer == 60)
+        {
+            objectRenderer.material.SetColor("_Color", terrainColor);
+        }
+
+        if (timer== 59)
+        {
+            objectRenderer.material.SetColor("_Color", Color.red);
+        }
+
+        if (timer == 49)
+        {
+            objectRenderer.material.SetColor("_Color", terrainColor);
+        }
+
+        if (timer == 39)
+        {
+            objectRenderer.material.SetColor("_Color", Color.red);
+        }
+
+        if (timer == 29)
+        {
+            objectRenderer.material.SetColor("_Color", terrainColor);
+        }
+
+        if (timer == 19)
+        {
+            objectRenderer.material.SetColor("_Color", Color.red);
+        }
+
+        if (timer == 9)
+        {
+            objectRenderer.material.SetColor("_Color", terrainColor);
+        }
+
+
+        if (timer== 0)
         {
             Destroy(gameObject);
         }
